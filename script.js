9009e6f9242d52958cff33cbe5b7ab9277b89860
vingoul <127652340+vingoul@users.noreply.github.com>
2025-01-25 16:25:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 var json = [
     {
-        "type": "FOTW",
-        "values": ["Japan", "Slovenia", "Sweden", "Czech Republic ", "Switzerland ", "Netherlands", "United Kingdom", "Hungary", "United Arab Emirates", "Austria", "Taiwan", "Romania", "Spain", "Andorra", "Gibraltar", "Sri Lanka", "India", "Liechtenstein", "Northern Ireland", "Estonia", "Finland", "Australia", "European Union", "Indonesia", "Croatia", "Germany", "Hong Kong", "England", "China", "Canada", "Bulgaria", "Cambodia", "Malaysia", "Kosovo", "Albania", "Montenegro", "Italy", "Poland", "Ukraine", "Laos", "Azores", "Norway", "Bosnia", "Chile", "Cyprus", "Greece", "Philippines", "Argentina", "South Korea", "French Guyana", "Aland Island", "ASEAN", "Turkey (Arranged)", "Cocos Island", "Thailand", "Costa Rica", "Ireland", "Dominican Republic (Arranged)", "Palau", "Portugal", "Azerbaijan", "USA", "Macau", "Iceland", "Belgium", "Latvia", "Kazakhstan", "Israel", "Benin", "Iran", "Ascension Island", "Falkland Island", "Nepal (Arranged)", "Serbia", "Slovakia", "Vietnam", "Uruguay", "Cuba", "Brazil", "Brunei", "Ivory Coast (Arranged)", "Rwanda", "Vatican City", "Singapore", "France", "Luxembourg", "Suriname", "Russia (Arranged)", "Malta (Arranged)", "Guadeloupe", "Uzbekistan (Arranged)", "Papua New Guineu", "Seychelles (Arranged)", "Bangladesh (Arranged)", "Peru", "North Macedonia", "Wales", "Belarus", "Bahamas", "Djibouti", "Lithuania", "Fiji", "Eswatini", "Moldova", "Jordan (Arranged)", "Oman", "San Marino", "Maldives", "Pitcairn Islands", "Belize", "Kyrgyzstan", "Georgia", "Mozambique", "New Zealand", "Comoros", "Nauru (Arranged)", "Angola", "Senegal", "South Africa", "Armenia", "Morocco"]
-    },
-    {
-        "type": "MOTW",
-        "values": ["Austria", "Costa Rica", "Canada", "UK", "Hong Kong", "Italy", "Finland", "Norway", "Spain", "Malaysia", "Japan", "Germany", "Czech Republic", "Taiwan", "China", "England", "Croatia", "Thailand", "UAE", "Sovereign Military Order Of Malta", "Poland", "Ukraine", "Armenia", "Portgual", "Latvia", "Turkey (Arranged)", "St Pierre and Miquelon", "Singapore", "Malta", "India (Arranged)", "Nepal", "Maldives", "Philippines", "Switzerland", "Slovenia", "Peru", "Belarus", "Bahamas", "Greece", "Hungary", "Ireland", "Cyprus", "Indonesia", "Pitcairn Islands", "San Marino", "Greenland", "Estonia", "Honduras", "Mozambique", "Denmark", "New Zealand ", "Israel", "USA (Arranged)", "France (Arranged)", "Nauru (Arranged)", "Mexico", "Egypt", "Macau"]
-
-    },
-    {
         "type": "Missing FOTW",
         "values": ["Afghanistan", "American Samoa (USA)", "Angola", "Anguilla (UK)", "Antigua and Barbuda", "Argentina", "Aruba (Netherlands)", "Azerbaijan", "Bahamas", "Bahrain", "Belize", "Benin", "Bermuda (UK)", "Bolivia", "Botswana", "Burkina Faso", "Burundi", "Cambodia", "Cayman Islands (UK)", "Central African Republic", "Chad", "Chile", "Cocos (Keeling) Islands (Australia)", "Colombia", "Comoros", "Costa Rica", "Cuba", "Cyprus", "Democratic Republic of the Congo", "Djibouti", "Dominica", "Dominican Republica", "Ecuador", "El Salvador", "Equatorial Guinea", "Ethiopia", "Fiji", "French Guiana (France)", "French Polynesia (France)", "Gabon", "Gambia", "Guadeloupe (France)", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Iran", "Iraq", "Isle of Man (UK)", "Ivory Coast", "Kiribati", "Kuwait", "Lebanon", "Lesotho", "Liberia", "Libya", "Madagascar", "Malawi", "Mali", "Malta", "Marshall Islands", "Martinique (France)", "Mauritania", "Mayotte (France)", "Mexico", "Montserrat (UK)", "Morocco", "Myanmar", "Nauru", "New Caledonia (France)", "Nicaragua", "Niger", "Nigeria", "Niue (NZ)", "Norfolk Island (Australia)", "North Korea", "Northern Mariana Islands (USA)", "Oman", "Palau", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Pitcairn Islands (UK)", "Puerto Rico", "Qatar", "Republic of the Congo", "Saint Barthelemy (France)", "Saint Kitts and Nevis", "Saint Lucia", "Saint Martin (France)", "Saint Pierre and Miquelon (France)", "Saint Vincent and the Grenadines", "Samoa", "Sao Tom and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sint Maarten (Netherlands)", "Solomon Islands", "Somalia", "South Sudan", "Sudan", "Suriname", "Svalbard and Jan Mayen (Norway)", "Swaziland", "Syria", "Tajikistan", "Tokelau (NZ)", "Tonga", "Turkey", "Turkmenistan", "Turks and Caicos Islands (UK)", "Tuvalu", "United Arab Emirates", "United States Virgin Islands (USA)", "Uruguay", "Vanuatu", "Wallis and Futuna (France)", "Yemen", "Zambia", "Zimbabwe", "SINT EUSTATIUS", "BONAIRE", "East Timor", "SABA", "Ascension", "British Indian Ocean Territory", "Saint Helena", "Eswatini"]
     },    
@@ -27,7 +18,44 @@ var json = [
     {
         "type": "Missing WOW Cuisines",
         "values": ["Greek", "Japanese", "Mexican", "Polish", "Russian", "Turkish", "Vietnamese"]
-    }
+    },
+    {
+        "type": "Missing World Travel by Nisan",
+        "values": ["Algeria", "Andorra", "Anguilla (UK)", "Argentina", "Armenia", "Azerbaijan", "Bosnia and Herzegovina", "Brunei", "Bulgaria", "Burundi", "Cambodia", "Cape Verde", "Colombia", "Cuba", "Democratic Republic of the Congo", "Dominican Republic", "Ecuador", "Egypt", "Georgia", "Gibraltar (UK)", "Iraq", "Jordan", "Kazakhstan", "Latvia", "Lithuania", "Madagascar", "Mexico", "Namibia", "North Korea", "Pakistan", "Palau", "Peru", "Romania", "Rwanda", "Saudi Arabia", "Serbia", "Tajikistan", "Tunisia", "Buen Camino (for WT)", "North Macedonia"]
+    },
+{
+"type": "Missing HPC",
+"values": ["Bulgaria", "China", "Denmark", "Dominican Republic", "Greece", "Hungary", "Kazakhstan", "Latvia", "Monaco", "Peru", "Puerto Rico", "Romania", "South Africa", "Sweden", "Thailand", "United Arab Emirates", "Vatican City", "Austria - 1st Edition", "Austria - 3rd Edition", "Australia - 2nd Edition", "Bavaria", "Lower Saxony", "Northern Germany", "Berlin - 2nd Edition", "Rhineland-Palatinate", "France - 2nd Edition", "France - 3rd Edition", "Finland - 3rd Edition", "Summer", "Online Meetup", "Meetup 1", "Meetup 2", "Happy Easter", "Planet Earth", "Happy Halloween", "Year of the Rabbit", "Happy New Year", "Merry Christmas", "Book Lover", "Earth Day", "HPC from the Sea", "Garden Lover", "Geocacher", "Pride", "Pott", "Peace", "2024", "Chocolate Lover", "Coffee Lover", "Happy Thanksgiving", "Merry Christmas 2nd Edition", "Postcard Collector", "Stargazer", "Tea Lover", "Women`s Day", "Year of the Dragon"]
+},
+{
+"type": "Missing FOTW EU – Pre Brexit",
+"values": ["Bulgaria", "Cyprus", "Hungary", "Latvia", "Lithuania", "Luxembourg", "Poland", "Romania", "Slovakia"]
+},
+{
+"type": "Missing FOTW EU – Post Brexit",
+"values": ["Bulgaria", "Cyprus", "Denmark", "Hungary", "Latvia", "Lithuania", "Luxembourg", "Slovakia", "Sweden"]
+},
+{
+"type": "Missing FOTW AU",
+"values": ["Algeria", "Angola", "Benin", "Botswana", "Burkina Faso", "Burundi", "Cameroon", "Cape Verde", "Central African Republic", "Chad", "Comoros", "Democratic Republic of the Congo", "Djibouti", "Egypt", "Equatorial Guinea", "Eritrea", "Ethiopia", "Gabon", "Gambia", "Ghana", "Guinea", "Guinea-Bissau", "Ivory Coast", "Lesotho", "Liberia", "Libya", "Madagascar", "Malawi", "Mali", "Mauritania", "Mauritius", "Morocco", "Mozambique", "Namibia", "Niger", "Nigeria", "Republic of the Congo", "Rwanda", "Sao Tom and Principe", "Senegal", "Seychelles", "Sierra Leone", "Somalia", "South Africa", "South Sudan", "Sudan", "Togo", "Tunisia", "Zambia", "Zimbabwe", "Eswatini", "Sahrawi Arab Democratic Republic"]
+},
+{
+"type": "Missing FOTW ASEAN",
+"values": ["Brunei", "Cambodia", "Laos", "Myanmar", "Vietnam"]
+},
+{
+"type": "Missing COTW",
+"values": ["Aland Islands (Finland)", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba (Netherlands)", "Bahamas", "Bahrain", "Belarus", "Belize", "Bosnia and Herzegovina", "Brazil", "Brunei", "Bulgaria", "Burundi", "Cambodia", "Cape Verde", "Chile", "Colombia", "Costa Rica", "Croatia", "Cuba", "Djibouti", "Egypt", "French Polynesia (France)", "Gambia", "Georgia", "Grenada", "Guam (USA)", "Guatemala", "Haiti", "Hungary", "Iceland", "Iran", "Israel", "Italy", "Jamaica", "Kazakhstan", "Kosovo", "Latvia", "Liechtenstein", "Macau (China)", "Madagascar", "Malta", "Mauritius", "Mexico", "Monaco", "Mongolia", "Morocco", "New Zealand", "Nicaragua", "Oman", "Panama", "Peru", "Puerto Rico", "Russia", "Saint Lucia", "San Marino", "Senegal", "Serbia", "Slovakia", "South Africa", "Sri Lanka", "Turkey", "Uganda", "Vatican City", "Azores", "Zanzibar"]
+},
+{
+"type": " Missing WorldCloud",
+"values" : ["Armenia", "Belarus", "Bosnia and Herzegovina", "Brunei", "Chile", "Colombia", "Croatia", "Democratic Republic of the Congo", "Egypt", "Iceland", "Iraq", "Madagascar", "Moldova", "Monaco", "New Zealand", "Rwanda", "Saudi Arabia", "Slovakia", "South Africa", "Trinidad and Tobago", "North Macedonia", "Peru 1 (Lima) ", "Peru 2 (Machu Picchu) ", "Belgium 1 (Brugge) "]
+},
+{
+"type": " Missing World Explorer",
+"values": ["Kazakhstan", "Moldova", "Mozambique", "Norway", "Singapore", "Ukraine", "United Kingdom"]
+}
+
 ]
 
 
